c1ed1f441fcd9cc84d985883f56ce94744a99a03
Life is Now <keylasan28@yahoo.com>
2020-06-05 21:06:37 -0400

    imports ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import react from "react";
 import "./App.css";
+import Search from "./components/Search";
+import Navbar from "./components/Navbar";
+import Results from "./components/Results";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+     <Router>
+<Navbar />
+     </Router>
+     
     </div>
   );
 }
