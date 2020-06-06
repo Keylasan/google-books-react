47668f1622609aee63a0963f4465a8535e1d4702
Life is Now <keylasan28@yahoo.com>
2020-06-06 00:32:59 -0400

    started index.js functions and query

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,34 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+import { search } from "../../index";
 import "./style.css";
 
 function Search() {
   return (
-       
-    <div className="search">
-    <div class="container">
-      <div class="row align-items-center my-5">
-        <div class="col-lg-7">
-          <img
-            class="img-fluid rounded mb-4 mb-lg-0"
-            src="http://placehold.it/900x400"
-            alt=""
-          />
-        </div>
-        <div class="col-lg-5">
-          <h1 class="font-weight-light">Search Your Favorite Books</h1>
-          <h2 class="font-weight-light"></h2>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book.
-          </p>
+    <div className="Search">
+      <div className="row">
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+          <input type="text" id="search-bar"></input>
+          <button onClick={search}>Search Book</button>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
-
 export default Search;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import axios from 'axios';
+
+export function search() {
+    var userInput = document.getElementById("search-bar").value;
+    var queryURL="https://www.googleapis.com/books/v1/volumes?q="+userInput;
+    return axios
+    .get(queryURL).then(res => {
+        console.log(res)
+        return res.data;
+    })
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
+
+
+
+
+
+
